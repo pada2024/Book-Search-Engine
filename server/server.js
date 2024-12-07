82d4eeefec5a20393228e4bddf1455da4c758823
Parisa Daily <misssweetardor@yahoo.com>
2024-12-07 11:08:49 -0800

    Refactored server.js to use apollo and GraphQL

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
+const { ApolloServer } = require('@apollo/server');
+const { authMiddleware } = require('./path/to/authMiddleware');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +14,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+}
+
+startApolloServer();
