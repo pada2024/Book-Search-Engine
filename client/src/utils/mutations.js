ff1661b03e200517853196185f2aeb95ffc10df7
Parisa Daily <misssweetardor@yahoo.com>
2024-12-08 22:42:30 -0800

    Added additional mutations to client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,5 @@
+import { gql } from '@apollo/client';
+
 export const LOGIN_USER = gql`
     mutation Login($username: String, $email: String, $password: String) {
   login(username: $username, email: $email, password: $password) {
@@ -5,3 +7,43 @@ export const LOGIN_USER = gql`
   }
 }
 `
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+      }
+    }
+  }
+`;
